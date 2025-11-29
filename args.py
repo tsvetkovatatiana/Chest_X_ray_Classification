0d38b761b29e3b4ac3ec052e7195b9c7dbf2d11c
Tatiana Tsvetkova <137390908+tsvetkovatatiana@users.noreply.github.com>
2025-11-29 22:12:05 +0200

    update default args

--- args.py
+++ args.py
@@ -3,12 +3,12 @@ import argparse
 def get_args():
     parser = argparse.ArgumentParser(description="Model training options")
 
-    parser.add_argument("-backbone", type=str, default="resnet18",
+    parser.add_argument("-backbone", type=str, default="resnet34",
                         choices=["resnet18", "resnet34", "resnet50"])
 
     parser.add_argument("-csv_dir", type=str, default="data/CSVs")
 
-    parser.add_argument("-batch_size", type=int, default=32,
+    parser.add_argument("-batch_size", type=int, default=16,
                         choices=[16, 32, 64])
 
     parser.add_argument("-output_dir", type=str, default="session")
